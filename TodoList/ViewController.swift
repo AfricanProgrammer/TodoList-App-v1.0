bb3690ceef6b1c7443292b587266ab0e68e3ede3
AfricanProgrammer <madzokeren@gmail.com>
2018-08-13 14:56:25 +0200

    A simple Todo List App
    A simple TODO List written in swift.
    This application is for demonstration of using labels, button, textview and the append functionality.This tutorial will be good for beginners trying to learn how to develop a simple application for iOS.
    
    Feel free to contribute to the project.

--- TodoList/ViewController.swift
+++ TodoList/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var addItem: UITextField!
+    @IBOutlet weak var textView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
     }
-
+    @IBAction func addButton(_ sender: Any) {
+        if let text = addItem.text {
+            if text == "" {
+                return
+            }
+            textView.text.append("\(text)\n")
+            addItem.text = ""
+            addItem.resignFirstResponder()
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
